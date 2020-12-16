83181e54a102cd1fab0fbc0dafa01c44098750c9
jCode319 <jdjack3@gmail.com>
2020-12-15 20:37:20 -0500

    seach table created

--- app/models/cocktail_bar.rb
+++ app/models/cocktail_bar.rb
@@ -1,3 +1,6 @@
 class CocktailBar < ApplicationRecord
   belongs_to :search
+  has_many :users, through: :reviews
+  has_many :reviews
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :cocktail_bar
+  has_many :reviews
+  has_many :cocktail_bars, through: :reviews
+
 end

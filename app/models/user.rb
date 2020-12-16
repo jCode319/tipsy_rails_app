43e5590937fb8d982958e8efa2039d3faf2da72e
jCode319 <jdjack3@gmail.com>
2020-12-15 20:48:26 -0500

    Migration successful

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :cocktail_bar
   has_many :reviews
   has_many :cocktail_bars, through: :reviews
-
+  has_many :searches
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_194911) do
+ActiveRecord::Schema.define(version: 2020_12_16_013317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cocktail_bars", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.float "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favs", force: :cascade do |t|
+    t.integer "user"
+    t.integer "cocktail_bar"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.float "rating"
+    t.bigint "user_id", null: false
+    t.bigint "cocktail_bar_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cocktail_bar_id"], name: "index_reviews_on_cocktail_bar_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "searches", force: :cascade do |t|
+    t.string "query"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_searches_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -26,4 +60,7 @@ ActiveRecord::Schema.define(version: 2020_12_15_194911) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "reviews", "cocktail_bars"
+  add_foreign_key "reviews", "users"
+  add_foreign_key "searches", "users"
 end

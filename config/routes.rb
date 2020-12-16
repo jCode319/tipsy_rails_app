55c13ad5c17b5178b4ed7f828bb4e3b9ff40c2ce
jCode319 <jdjack3@gmail.com>
2020-12-16 16:40:27 -0500

    Modify route to include signup. Also created seed file for testing in console.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+
+  root 'sessions#home'
+
+  get '/signup' => 'sessions#new'
+  post '/signup' => 'sessions#create'
+
   resources :favs
   resources :reviews
   resources :cocktail_bars
-  root 'pages#home'
   resources :users
+  resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Review.destroy_all
+Fav.destroy_all
+CocktailBar.destroy_all
+
+don = User.create(username: "Don", email: "Don@me.com", city: "Chicago", state: "IL", password_digest: 1234567899, favorite_cocktail: "Mojito")
+lisa = User.create(username: "Lisa", email: "Lisa@me.com", city: "Chicago", state: "IL", password_digest: 9987654321, favorite_cocktail: "Cosmo")
+
+rockbottom = CocktailBar.create(name: "Rock Bottom", address: "123 state street", rating: "3 stars")
+barlouie = CocktailBar.create(name: "Bar Louie", address: "123 main street", rating: "4 stars")
+
+rb_review = Review.create(content: "Bad service", rating: 1, user_id: 1, cocktail_bar_id: 1)
+bl_review = Review.create(content: "Had a blast", rating: 5, user_id: 2, cocktail_bar_id: 2)
+
+don_fav = Fav.create(user_id: 1, cocktail_bar_id: 2)
+lisa_fav = Fav.create(user_id: 2, cocktail_bar_id: 1) 

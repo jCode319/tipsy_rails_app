0f59ec07df11ca58dc9a446c4f4371df835f504d
jCode319 <jdjack3@gmail.com>
2020-12-20 18:08:36 -0500

    added a title attr to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_020242) do
+ActiveRecord::Schema.define(version: 2020_12_20_230026) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2020_12_19_020242) do
   end
 
   create_table "reviews", force: :cascade do |t|
+    t.string "title"
     t.string "content"
     t.float "rating"
     t.bigint "user_id", null: false

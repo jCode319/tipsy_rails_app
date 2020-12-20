80a00fa452cff2b8b07820a83926794214c82025
jCode319 <jdjack3@gmail.com>
2020-12-20 16:17:50 -0500

    added attr to CocktailBars model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_013317) do
+ActiveRecord::Schema.define(version: 2020_12_19_020242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,10 @@ ActiveRecord::Schema.define(version: 2020_12_16_013317) do
     t.float "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "phone_number"
+    t.string "yelp_url"
+    t.string "image_url"
+    t.integer "yelp_id"
   end
 
   create_table "favs", force: :cascade do |t|

7accfcc67f706c8056798bf6f0fc892a2f71bbac
jCode319 <jdjack3@gmail.com>
2020-12-21 14:53:34 -0500

    minor updates made to reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,21 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :cocktail_bar
-end
+
+
+# def self.rating(float)
+#         case float
+#         when 1..1.5
+#             "*"
+#         when 2..2.5
+#             "**"
+#         when 3..3.5
+#             "***"
+#         when 4..4.5
+#             "****"
+#         when 5..5.5
+#             "*****"
+#         end
+#     end
+
+  end

3244a7e9d834cfe612fddae59d68c86083643ba9
jCode319 <jdjack3@gmail.com>
2020-12-21 14:53:07 -0500

    favs controller create and destroy methods created

--- app/controllers/favs_controller.rb
+++ app/controllers/favs_controller.rb
@@ -1,2 +1,30 @@
 class FavsController < ApplicationController
+  before_action :redirect_if_not_logged_in
+
+  def new
+    @fav = Fav.new
+  end
+
+  def create
+    byebug
+    @cocktail_bar = CocktailBar.first
+    @fav = current_user.favs.build(set_fav)
+    @fav[:cocktail_bar_id] = @cocktail_bar[:id]
+    if @fav.save
+      redirect_to current_user
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    byebug
+    @fav = Fav.find_by
+    @fav.destroy
+    redirect_to current_user
+  end
+
+  private
+
+
 end

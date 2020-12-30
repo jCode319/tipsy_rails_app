1fcd70a5fd59ee183de9f3e007ff467abcb803f4
jCode319 <jdjack3@gmail.com>
2020-12-29 20:28:54 -0500

    make favs is working, including nav bar and links to delete favorites

--- app/controllers/favs_controller.rb
+++ app/controllers/favs_controller.rb
@@ -1,30 +1,37 @@
 class FavsController < ApplicationController
   before_action :redirect_if_not_logged_in
 
+  def index
+    @favs = Fav.all
+  end
+
   def new
-    @fav = Fav.new
+    if params[:cocktail_bar_id] && @cocktail_bar = CocktailBar.find_by_id(params[:cocktail_bar_id])
+      @fav = @cocktail_bar.favs.build
+    else
+      @error = "That fav doesn't exist" if params[:cocktail_bar_id]
+      @fav = Fav.new
+    end
   end
 
   def create
-    byebug
-    @cocktail_bar = CocktailBar.first
-    @fav = current_user.favs.build(set_fav)
+    @cocktail_bar = CocktailBar.find(params[:cocktail_bar_id])
+    @fav = current_user.favs.build
     @fav[:cocktail_bar_id] = @cocktail_bar[:id]
     if @fav.save
-      redirect_to current_user
+      redirect_to favs_path
     else
-      render :new
+      flash[:error] = "We were unable to add this location to your favorites, please try again"
+      redirect_to current_user
     end
   end
 
   def destroy
-    byebug
-    @fav = Fav.find_by
+    @fav = Fav.find_by(id: params[:id])
     @fav.destroy
     redirect_to current_user
   end
 
   private
-
 
 end
